e6922ae357b37a8b01b43e8d96769e5963b277c8
moisesventtura <moisesventura15@gmail.com>
2021-06-07 21:03:15 -0300

    CrudcargoService criado + método salvar

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,19 +1,25 @@
 package br.com.alura.spring.data;
 
+import br.com.alura.spring.data.service.CrudCargoService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.alura.spring.data.orm.Cargo;
 import br.com.alura.spring.data.repository.CargoRepository;
+import org.yaml.snakeyaml.emitter.ScalarAnalysis;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
 
-	private final CargoRepository repository;
+	private Boolean system = true;
+
+	private final CrudCargoService cargoService;
 	
-	public SpringDataApplication(CargoRepository repository) {
-		this.repository = repository;
+	public SpringDataApplication(CrudCargoService cargoService) {
+		this.cargoService = cargoService;
 	}
 	
 	public static void main(String[] args) {
@@ -22,10 +28,20 @@ public class SpringDataApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Cargo cargo = new Cargo();
-		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
-		
-		repository.save(cargo);
+		Scanner scanner = new Scanner(System.in);
+
+		while(system){
+			System.out.println("Qual acao voce quer executar");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Cargo");
+
+			int action = scanner.nextInt();
+			if(action == 1){
+				cargoService.incial(scanner);
+			}else{
+				system = false;
+			}
+		}
 	}
 
 }
